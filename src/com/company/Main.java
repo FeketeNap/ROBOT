7ab5d8def6ea6397e1898c3884faaba2f8380a26
daria <dfrulenkova@gmail.com>
2019-02-10 18:46:19 +0200

    Реализация коман для робота

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,23 +11,28 @@ public class Main {
     public static int indexJ;
     public static String Mot;
     public static Robot Robott = new Robot();
+    public static int[] startPos = new int[4];
+
 
 
     public static void main(String[] args) {
 
-
-
-
-
         Robott.start(field);
+
+        startPos[0] = indexI;
+        startPos[1] = indexJ;
+
+        System.out.println("Starting position: ");
+
         printArray(field);
 
         motion(Mot,Robott);
-
+        check(startPos);
 
 
     }
     private static void printArray(int[][] arr) {
+        System.out.println(" ");
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 System.out.print(arr[i][j] + " ");
@@ -38,45 +43,48 @@ public class Main {
     private static void motion (String str, Object r){
 
         Mot = str;
-        r = Robott;
 
+        System.out.println("You must enter the characters: 'R' or 'L' or 'U' or 'D':");
 
         Scanner scanner = new Scanner(System.in);
         str = scanner.nextLine();
-        str.toUpperCase();
+        String s = str.toUpperCase();
 
-      char[] c = str.toCharArray();
+        char[] c = s.toCharArray();
         for (int i = 0; i < c.length; i++) {
             if(c[i] == 'R'){
                 Robott.right(field,indexJ);
-                System.out.println(" ");
-                printArray(field);
             }
             else if(c[i] == 'L'){
                 Robott.left(field,indexJ);
-                System.out.println(" ");
-                printArray(field);
             }
             else if(c[i] == 'U'){
                 Robott.up(field,indexI);
-                System.out.println(" ");
-                printArray(field);
             }
             else if(c[i] == 'D'){
                 Robott.down(field,indexI);
-                System.out.println(" ");
-                printArray(field);
             }
             else{
                 System.out.println("You must enter the characters: 'R' or 'L' or 'U' or 'D'");
-
             }
-
         }
+        System.out.println("Coordinates now: ");
+        printArray(field);
+        System.out.println(" ");
 
-      System.out.println(Arrays.toString(c));
+    }
+    private static void check (int [] startPos){
+        startPos[2] = indexI;
+        startPos[3] = indexJ;
 
 
+        if (startPos[0] == startPos[2] && startPos[1] == startPos[3]){
+
+            System.out.println("coordinates coincide!");
+        }else{
+
+            System.out.println("coordinates do not match!");
+        }
 
     }
 }
